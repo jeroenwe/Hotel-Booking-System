75d325fd2b7d07075a0164cb99d3605a0b5e54a1
Weerstra, Jeroen <jeroenwe@youforce.net>
2026-06-11 15:27:37 +0200

    Added Booking class

---
+++ HotelBookingSystem.Console/Models/Booking.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelBookingSystem.ConsoleApp.Models;
+
+public class Booking(string customerId, int nights)
+{
+    public string CustomerId { get; set; } = customerId;
+
+    public int Nights { get; set; } = nights;
+
+    public DateTime BookingDate { get; set; } = DateTime.Now;
+
+    public bool IsCanceled { get; set; } = false;
+}
+
